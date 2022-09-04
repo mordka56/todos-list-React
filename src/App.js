fc5fdecae382d08fb98f8b12eba763d56339cf3d
Jarosław Bogdański <mordka4428@gmail.com>
2022-09-04 12:58:03 +0200

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -21,22 +21,38 @@ function App() {
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
-  return (
 
-    <Container>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
-      <Section
-        title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
-      />
-    </Container>
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return(
+        { ...task, done: !task.done })
 
-  );
+}
+return task;
+    }));
+  }
+return (
+
+  <Container>
+    <Header title="Lista zadań" />
+    <Section
+      title="Dodaj nowe zadanie"
+      body={<Form />}
+    />
+    <Section
+      title="Lista zadań"
+      body={<Tasks 
+        tasks={tasks} 
+        hideDone={hideDone} 
+        removeTask={removeTask}
+        toggleTaskDone={toggleTaskDone}
+        />}
+      extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
+    />
+  </Container>
+
+);
 }
 
 export default App;
